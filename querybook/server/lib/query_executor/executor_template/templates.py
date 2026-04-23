3024b0ec94de4f4262fa0f52b379032f411f6ec2
jiaxuan.lee <jiaxuan.lee@shopline.com>
2026-04-23 15:43:43 +0800

    trion add impersonate switch

--- querybook/server/lib/query_executor/executor_template/templates.py
+++ querybook/server/lib/query_executor/executor_template/templates.py
@@ -74,6 +74,7 @@ trino_executor_template = StructFormField(
     ),
     ("username", FormField(regex="\\w+")),
     ("password", FormField(hidden=True)),
+    ("impersonate", FormField(field_type=FormFieldType.Boolean)),
     (
         "proxy_user_id",
         FormField(
